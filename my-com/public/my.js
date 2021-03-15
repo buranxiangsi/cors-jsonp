aecf57700d6adb02ec72b59980fc909399b336fa
buranxiangsi <l13789060033@163.com>
2021-03-15 18:20:12 +0800

    封装jsonp

--- my-com/public/my.js
+++ my-com/public/my.js
@@ -1,17 +1,26 @@
-// CORS 访问
-// const request = new XMLHttpRequest()
-// request.open('GET', 'http://qq.com:8888/friends.json')
-// request.onreadystatechange = () => {
-//     if (request.readyState === 4 && request.status === 200) {
-//         console.log(request.response)
-//     }
-// // }
-// request.send()
+const { resolve } = require("node:path")
 
-const script = document.createElement('script')
-script.src = 'http://qq.com:8888/friends.js'
-script.onload = () => {
-    console.log(window.xxx)
+function jsonp(url) {
+    return new Promise((resolve, reject) => {
+        const random = Math.random()
+        window[random] = (data) => {
+            resolve(data)
+        }
+        const script = document.createElement('script')
+        script.src = `${url}?callback=${random}`
+        script.onload = () => {
+            script.remove()
+        }
+        script.onerror = () => {
+            reject()
+        }
+        document.body.appendChild(script)
+    })
 }
-document.body.appendChild(script)
+
+
+jsonp('http://qq.com:8888/friends.js')
+    .then((data) => {
+        console.log(data)
+    })
 
--- qq-com/server.js
+++ qq-com/server.js
@@ -39,13 +39,19 @@ var server = http.createServer(function (request, response) {
         response.end()
 
     } else if (path === '/friends.js') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-        const string = fs.readFileSync('./public/friends.js').toString()
-        const data = fs.readFileSync('./public/friends.json').toString()
-        const string2 = string.replace('{{ data }}', data)
-        response.write(string2)
-        response.end()
+        if (request.headers['referer'.indexOf('http://my.com:9999')]) {
+            response.statusCode = 200
+            response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+            const string = `window['{{xxx}}']({{ data }})`
+            const data = fs.readFileSync('./public/friends.json').toString()
+            const string2 = string.replace('{{ data }}', data).replace('{{xxx}}', query.callback)
+            response.write(string2)
+            response.end()
+
+        } else {
+            response.statusCode = 404
+            response.end()
+        }
     } else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
